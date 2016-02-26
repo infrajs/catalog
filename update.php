ef41475c26bac7e53609f27ed215c81efa22d472
Антон Кияткин <a.kiyatkin@itlf.ru>
2016-02-26 04:31:38 +0400

    support auto update

--- update.php
+++ update.php
@@ -2,6 +2,5 @@
 namespace infrajs\autoedit;
 use infrajs\path\Path;
 require_once(__DIR__.'/../../../vendor/autoload.php');
-require_once(__DIR__.'/../path/install.php');
 
-Path::mkdir($dirs['data'].'.marks/');
+Path::mkdir('~.marks/');
