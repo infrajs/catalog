f992c1b832581f311ed64edaa86fe64426531660
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2017-09-29 10:19:48 +0400

    fix seo for root

--- search.php
+++ search.php
@@ -38,7 +38,7 @@ if(isset($_GET['seo'])){
 	
 	unset($ans['md']);
 	unset($ans['m']);
-	$ans['external']  =  '-catalog/seo.json';
+	$ans['external']  =  '~catalog/seo.json';
 	$ans['canonical']  =  View::getPath().$link;
 	
 	return Ans::ans($ans);
